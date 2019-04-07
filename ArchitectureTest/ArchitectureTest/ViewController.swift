9b7e9f905768dd9a105192ebef42bd71d38c131f
jLaszczewski <j.laszczewski@icloud.com>
2019-04-07 21:33:15 +0200

    Initial commit.

---
+++ ArchitectureTest/ArchitectureTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureTest
+//
+//  Created by Jakub Łaszczewski on 07/04/2019.
+//  Copyright © 2019 letUs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
